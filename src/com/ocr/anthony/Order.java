071cc32297b20c1adaeb7d68c37f29f4024745af
Mustaphabdt <boudount@gmail.com>
2019-02-18 11:51:46 +0100

    create on order fonction nbMenu and transfer main code on nbMenu; for optimise the Main

--- src/com/ocr/anthony/Order.java
+++ src/com/ocr/anthony/Order.java
@@ -1,10 +1,18 @@
 package com.ocr.anthony;
 
+import java.util.Scanner;
+
 public class Order {
     /**
-     * Display all available menus in the restaurant.
+     * Display all available  menus  in  the restaurant.
      */
+
     public void displayAvailableMenu() {
+        System.out.println("Choix menu:");
+        System.out.println("1 - poulet");
+        System.out.println("2 - boeuf");
+        System.out.println("3 - végétarien");
+        System.out.println("Que souhaitez-vous comme menu ? ");
 
     }
     /**
@@ -12,6 +20,23 @@ public class Order {
      * @param nbMenu The selected menu.
      */
     public void displaySelectedMenu(int nbMenu) {
-
+        if (nbMenu == 1) {
+            System.out.println("Vous avez choisi comme menu : poulet");
+        } else if (nbMenu == 2){
+            System.out.println("Vous avez choisi comme menu : boeuf");
+        } else if ( nbMenu == 3) {
+            System.out.println("Vous avez choisi comme menu : végétarien");
+        } else {
+            System.out.println("Vous n'avez pas choisi de menu parmi les choix proposés");
+        }
+    }
+    /**
+     * Run asking process for a menu.
+     */
+    public void runMenu(){
+        this.displayAvailableMenu();
+        Scanner sc = new Scanner(System.in);
+        int nb = sc.nextInt();
+        this.displaySelectedMenu(nb);
     }
 }
